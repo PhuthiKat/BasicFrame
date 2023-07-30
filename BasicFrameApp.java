39e3b5c3a09a349c1143f9b34a0a97d89ac77d65
PhuthiKat <108768362+PhuthiKat@users.noreply.github.com>
2023-07-30 12:41:24 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,19 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+public class BasicFrameApp
+{
+
+    public static void main(String[] args) 
+    {
+        // declare variable for library
+      MyFirstFrame myFrame;
+      
+       //instantiate the frame
+      myFrame = new MyFirstFrame();
+    }
+    
+}
